01f4be6b3aebd0b6ae08e5b62cc7d06066f02821
Filip Fajfer <filipfajfer5@gmail.com>
2024-08-06 16:29:59 +0200

    feat: searchbar

---
+++ frontend/src/src/pages/homepage/index.js
@@ -0,0 +1,9 @@
+import SearchBar from "../../components/search-bar";
+
+export default function Homepage() {
+    return (
+        <div>
+            <SearchBar></SearchBar>
+        </div>
+    );
+}
