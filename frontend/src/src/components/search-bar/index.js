5bf5f92220a3f3d13ff1254a0640d978813182f9
Filip Fajfer <filipfajfer5@gmail.com>
2024-08-06 16:31:18 +0200

    fix: searchbar styling

--- frontend/src/src/components/search-bar/index.js
+++ frontend/src/src/components/search-bar/index.js
@@ -1,12 +1,12 @@
-import './index.css'
+import './index.css';
 
 export default function SearchBar() {
     return (
-        <div>
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="button-addon2"></input>
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        <div className="search-bar">
+            <div className="input-group">
+                <input type="text" className="form-control" placeholder="Search..." aria-label="Search" aria-describedby="button-addon2"></input>
+                <button className="btn search-btn" type="button" id="button-addon2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                     </svg>
                 </button>
