566e255dd343dc534abeef3f6592576c9f0953f2
Filip Fajfer <filipfajfer5@gmail.com>
2024-08-06 17:38:29 +0200

    feat: Homepage finished

--- frontend/src/src/pages/homepage/index.js
+++ frontend/src/src/pages/homepage/index.js
@@ -1,11 +1,15 @@
+import './index.css'
+
 import SearchBar from "../../components/search-bar";
+import logo from '../../chopchopgo.png'
 
 export default function Homepage() {
     return (
         <div className="container">
-            <div className="row justify-content-center align-items-start" style={{ height: '80vh', marginTop: '40vh' }}>
+            <div className="row justify-content-center align-items-start" style={{ height: '80vh', marginTop: '20vh' }}>
                 <div className="col-md-8 col-lg-6">
                     <div className="text-center p-4 rounded">
+                        <img src={logo} alt="Logo" className="circle-image mb-4" style={{ maxWidth: '50%', height: 'auto' }} />
                         <SearchBar />
                     </div>
                 </div>
