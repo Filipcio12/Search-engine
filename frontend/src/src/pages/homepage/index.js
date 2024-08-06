5252775d1de17606b174576fbc1cf0e14a2a06f0
Filip Fajfer <filipfajfer5@gmail.com>
2024-08-06 17:18:08 +0200

    fix: homepage styling

--- frontend/src/src/pages/homepage/index.js
+++ frontend/src/src/pages/homepage/index.js
@@ -2,8 +2,14 @@ import SearchBar from "../../components/search-bar";
 
 export default function Homepage() {
     return (
-        <div>
-            <SearchBar></SearchBar>
+        <div className="container">
+            <div className="row justify-content-center align-items-start" style={{ height: '80vh', marginTop: '40vh' }}>
+                <div className="col-md-8 col-lg-6">
+                    <div className="text-center p-4 rounded">
+                        <SearchBar />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
