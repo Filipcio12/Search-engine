6faac6bd4eb0b984c841f9936b57700cb5aed9bf
Filip Fajfer <filipfajfer5@gmail.com>
2024-08-07 23:50:45 +0200

    feat: indexer

---
+++ backend/src/indexer/indexer.py
@@ -0,0 +1,28 @@
+import string
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class Indexer():
+    def __init__(self, lang='english') -> None:
+        self.vectorizer = TfidfVectorizer(stop_words=lang)
+        self.tfidf_matrix = []
+        self.filenames = []
+
+    def __preprocess(self, text: str) -> str:
+        text = text.lower()
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        return text
+
+    def index_documents(self, documents: list[str], filenames: list[str]):
+        processed_documents = [self.__preprocess(doc) for doc in documents]
+        self.tfidf_matrix = self.vectorizer.fit_transform(processed_documents)
+        self.filenames = filenames
+
+    def search(self, query, vectorizer, tfidf_matrix, top_n=5):
+        query_vec = vectorizer.transform([query])
+        cosine_similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
+        most_similar_docs = np.argsort(cosine_similarities)[::-1][:top_n]
+        results = [(self.filenames[i], cosine_similarities[i]) for i in most_similar_docs]
+        return results
