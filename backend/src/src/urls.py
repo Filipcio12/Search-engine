2dde6eb1d0aceb483d934a87e918655462adb1f3
Filip Fajfer <filipfajfer5@gmail.com>
2024-08-09 19:34:47 +0200

    feat: add search to urls

--- backend/src/src/urls.py
+++ backend/src/src/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from indexer.views import SearchIndexerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', SearchIndexerView.as_view())
 ]
