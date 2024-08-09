dee97d3f47a6f937ebc14ee6d294e088b1360140
Filip Fajfer <filipfajfer5@gmail.com>
2024-08-09 20:44:09 +0200

    fix: transformed results in the search endpoint

--- backend/src/indexer/views.py
+++ backend/src/indexer/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.apps import apps
+from crawler.models import ScrapingResult
 
 URLS_PER_PAGE = 10
 
@@ -13,6 +14,19 @@ class SearchIndexerView(APIView):
 
         if query:
             results = indexer.search(query, URLS_PER_PAGE)
-            return Response({'results': results}, status=status.HTTP_200_OK)
+            transformed_results = []
+
+            for result in results:
+                filename, similarity = result
+                page = ScrapingResult.objects.get(url=filename)
+                content = ' '.join(page.content.split()[:20])
+                transformed_results.append({
+                    'url': filename,
+                    'title': page.title,
+                    'content': content,
+                    'similarity': similarity,
+                })
+
+            return Response({'results': transformed_results}, status=status.HTTP_200_OK)
         else:
             return Response({'ERROR: No query provided.'}, status=status.HTTP_400_BAD_REQUEST)
