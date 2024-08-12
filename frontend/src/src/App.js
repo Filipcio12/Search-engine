69586acc68b293ee7ff6725eed8449a5554e985a
Filip Fajfer <filipfajfer5@gmail.com>
2024-08-12 14:53:20 +0200

    feat: init routing

--- frontend/src/src/App.js
+++ frontend/src/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-
 import Homepage from './pages/homepage';
 import SearchPage from './pages/searchpage';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <SearchPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="/search" element={<SearchPage />}/>
+      </Routes>
+    </Router>
   );
 }
 
