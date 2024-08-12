742a528f60f0de65af63038b2cb928158e4b5e60
Filip Fajfer <filipfajfer5@gmail.com>
2024-08-12 14:58:55 +0200

    fix: remove bloat

--- frontend/src/src/pages/searchpage/index.js
+++ frontend/src/src/pages/searchpage/index.js
@@ -6,21 +6,13 @@ import './index.css';
 export default function SearchPage() {
     const [results, setResults] = useState([]);
 
-    const handleSearchResults = (data) => {
-        setResults(data);
-    };
-
-    const urlCards = Array.from({ length: 10 }, (_, index) => (
-        <UrlCard key={index} />
-    ));
-
     return (
         <div>
             <div className="bg-dark text-white p-3 mb-3">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 ms-3">
-                            <SearchBar onSearch={handleSearchResults}/>
+                            <SearchBar onSearch={setResults}/>
                         </div>
                     </div>
                 </div>
